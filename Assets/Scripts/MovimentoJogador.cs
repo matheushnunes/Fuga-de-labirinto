d47d9c923bc6be5a70956cccf39eee16b00d47af
Matheus Henrique <matheushnunes2005@gmail.com>
2025-10-19 02:49:13 -0300

    Update MovimentoJogador.cs

--- Assets/Scripts/MovimentoJogador.cs
+++ Assets/Scripts/MovimentoJogador.cs
@@ -4,6 +4,7 @@ public class MovimentoJogador : MonoBehaviour
 {
     Rigidbody2D rb;
     float direcaoMovimento; // <-- Vamos guardar -1 (esquerda), 1 (direita) ou 0 (parado)
+    bool querPular = false;
 
     void Start()
     {
@@ -24,10 +25,21 @@ public class MovimentoJogador : MonoBehaviour
         {
             direcaoMovimento = 0f; // <-- Não estamos pressionando nada, ficar parado
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)){
+            querPular = true;
+        }
     }
     void FixedUpdate()
     {
         // Aplicamos a força baseada na direção que o Update() capturou
         rb.AddForce(Vector2.right * direcaoMovimento * 10f);
+
+        if (querPular == true)
+        {
+            // Aplicamos o pulo como um "Impulso" (um chute instantâneo)
+            rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse); 
+            querPular = false; // <-- Abaixa a bandeira para não pular de novo
+        }
     }
 }
