2c6f4d64bc1eb3f2a4dd0c3a6d766d3540bdb72d
renatakaren <renatakarenr@gmail.com>
2025-10-19 01:51:53 -0300

    Update MovimentoJogador.cs

--- Assets/Scripts/MovimentoJogador.cs
+++ Assets/Scripts/MovimentoJogador.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
 
-public class MovimentoJogador : MonoBehaviour{
+public class MovimentoJogador : MonoBehaviour
+{
+    Rigidbody2D rb; // <-- 1. Declaramos a variável aqui
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // <-- 2. Inicializamos a variável aqui
+    }
 
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector2.right * 10f); // <-- Adiciona uma força para a direita
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector2.left * 10f); // <-- Adiciona uma força para a esquerda
+        }
+    }
 }
-    
